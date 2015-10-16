3bef79228b67b0e5c680595d0bdc87770f6bb208
MoeSadoon <msadoon590@gmail.com>
2015-10-16 14:26:54 +0100

    fixed delete review issue, now need to pass endorsement link test

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,12 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
 
-  resources :restaurants do
-    resources :reviews
+  resources :restaurants, shallow:true  do
+    resources :reviews do
+      resources :endorsements
+    end
   end
+
   get 'restaurants' => 'restaurants#index'
 
   # get '/restaurants/new' => 'restaurants#new'
