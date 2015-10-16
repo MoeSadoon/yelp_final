11880c5d7f16a79714019ebc3fa11c28a3bb3867
MoeSadoon <msadoon590@gmail.com>
2015-10-16 17:40:24 +0100

    all basic user functions work, but images not showing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :user
-  has_many :endorsements
+  has_many :endorsements, dependent: :delete_all
   validates :rating, inclusion: (1..5)
   validates :user, uniqueness: { scope: :restaurant, message: 'has reviewed this restaurant already' }
 end
